c956fa3f2a9a2132846f7ef5ae95c2d640d68bfc
hp0404 <hryhorii.pavlenko@gmail.com>
2023-09-27 10:51:41 +0300

    tabs

--- apps-scripts/main.js
+++ apps-scripts/main.js
@@ -2,28 +2,29 @@ function duplicateSheet() {
     var sheetNames = ["A", "B", "C"];
   
     for (const sheetName of sheetNames) {
-        var spreadsheet = SpreadsheetApp.getActive();
-        spreadsheet.duplicateActiveSheet();
-        spreadsheet.getActiveSheet().setName(sheetName);
+      var spreadsheet = SpreadsheetApp.getActive();
+      spreadsheet.duplicateActiveSheet();
+      spreadsheet.getActiveSheet().setName(sheetName);
     }
-}
+  }
   
-function duplicateAndProtect() {
+  function duplicateAndProtect() {
     var sheetNames = ["A", "B", "C"];
-
+  
     for (const region of sheetNames) {
-        var spreadsheet = SpreadsheetApp.getActive();
-        spreadsheet.duplicateActiveSheet();
-        spreadsheet.getActiveSheet().setName(region);
-        var sheet = spreadsheet.getActiveSheet();
-        sheet
+      var spreadsheet = SpreadsheetApp.getActive();
+      spreadsheet.duplicateActiveSheet();
+      spreadsheet.getActiveSheet().setName(region);
+      var sheet = spreadsheet.getActiveSheet();
+      sheet
         .getRange(
-            spreadsheet.getCurrentCell().getRow(),
-            1,
-            1,
-            sheet.getMaxColumns(),
+          spreadsheet.getCurrentCell().getRow(),
+          1,
+          1,
+          sheet.getMaxColumns(),
         )
         .activate();
-        spreadsheet.getActiveRange().protect();
+      spreadsheet.getActiveRange().protect();
     }
-}
+  }
+  
