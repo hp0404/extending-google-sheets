c83a7d264ea69cc5f9d553a8f0bbdba44e690842
hp0404 <hryhorii.pavlenko@gmail.com>
2023-09-27 10:48:51 +0300

    duplicating sheets

---
+++ apps-scripts/main.js
@@ -0,0 +1,29 @@
+function duplicateSheet() {
+    var sheetNames = ["A", "B", "C"];
+  
+    for (const sheetName of sheetNames) {
+      var spreadsheet = SpreadsheetApp.getActive();
+      spreadsheet.duplicateActiveSheet();
+      spreadsheet.getActiveSheet().setName(sheetName);
+    }
+  }
+  
+  function duplicateAndProtect() {
+    var sheetNames = ["A", "B", "C"];
+  
+    for (const region of sheetNames) {
+      var spreadsheet = SpreadsheetApp.getActive();
+      spreadsheet.duplicateActiveSheet();
+      spreadsheet.getActiveSheet().setName(region);
+      var sheet = spreadsheet.getActiveSheet();
+      sheet
+        .getRange(
+          spreadsheet.getCurrentCell().getRow(),
+          1,
+          1,
+          sheet.getMaxColumns(),
+        )
+        .activate();
+      spreadsheet.getActiveRange().protect();
+    }
+  }
